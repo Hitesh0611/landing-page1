451a63ecef88e4737c0df7e66dd91f31e8fc0b68
hitesh0611 <chaudharihitesh0611@gmail.com>
2024-07-19 20:49:13 +0530

    landing page

---
+++ script.js
@@ -0,0 +1,7 @@
+function instagram() {
+    window.location.href = "https://www.instagram.com/hitesh0611__", "_blank";
+}
+
+function github() {
+    window.location.href = "https://github.com/Hitesh0611", "_blank"
+}
